b2b3452ec4c7dc9b871785b3a99158c3e9c87f91
Anton Gerber <cagerber@gmail.com>
2015-06-30 05:34:38 +0200

    Initial Commit

---
+++ app.js
@@ -0,0 +1,15 @@
+
+var user = require("./lib/user");
+var otherUser = require("./lib/user");
+
+console.log(user.getName());
+console.log(otherUser.getName());
+
+user.setName("Steve");
+
+console.log(otherUser.getName());
+/*
+var utility = require("./lib/utility/models/utility");
+
+utility.sayHi();
+*/
